a61552b17826b3eb85ea76324b6ea5b661f54856
Suzysheep1106 <115514214+Suzysheep1106@users.noreply.github.com>
2022-10-24 20:18:35 +0800

    Create GlobalExceptionHandler.java
    创建全局异常处理器

---
+++ src/main/java/com/cxaou/thetestsystem/common/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.cxaou.thetestsystem.common;
+
+import com.cxaou.thetestsystem.erro.ParameterException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@Slf4j
+@ResponseBody
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)  // 要处理的异常
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
+
+        String message = ex.getMessage();
+        log.error(message);
+        if (message.contains("Duplicate entry")){
+            String[] s = message.split(" ");
+            String msg = s[2]+ "已存在";
+            return  R.error(msg);
+        }
+        return R.error("未知错误");
+
+    }
+
+    @ExceptionHandler(ParameterException.class)  // 要处理的异常
+    public R<String> exceptionHandler(ParameterException ex){
+        return R.error(ex.getMessage());
+
+    }
+}

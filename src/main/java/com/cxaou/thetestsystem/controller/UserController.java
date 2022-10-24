39bbef7e99c3740059566caaf5915855a04d11f6
cxaou <2576353248@qq.com>
2022-10-24 15:01:22 +0800

    编写登录方法

--- src/main/java/com/cxaou/thetestsystem/controller/UserController.java
+++ src/main/java/com/cxaou/thetestsystem/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.cxaou.thetestsystem.controller;
 
-import io.swagger.annotations.Api;
+import com.cxaou.thetestsystem.common.R;
+import com.cxaou.thetestsystem.pojo.User;
+import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 public class UserController {
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "phone", value = "手机号", paramType = "from"),
+            @ApiImplicitParam(name = "username", value = "用户名", paramType = "from"),
+            @ApiImplicitParam(name = "password",value = "密码",paramType = "from",required = true),
+            @ApiImplicitParam(name = "type",value = "登录方式 0 手机号登录 1 用户名登录",required = true)
+    })
+    @PostMapping
+    public R<User> login(@RequestBody User user) {
 
+        return null;
+    }
 }
--- src/main/java/com/cxaou/thetestsystem/pojo/User.java
+++ src/main/java/com/cxaou/thetestsystem/pojo/User.java
@@ -1,16 +1,19 @@
 package com.cxaou.thetestsystem.pojo;
 
-import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+import java.io.Serializable;
 
 /**
  * 
  * @TableName user
  */
+@ApiModel("学生信息表")
 @TableName(value ="user")
 @Data
 public class User implements Serializable {
@@ -18,36 +21,43 @@ public class User implements Serializable {
      * 主键
      */
     @TableId
+    @ApiModelProperty("id")
     private Long id;
 
     /**
      * 手机号
      */
+    @ApiModelProperty("手机号")
     private String phone;
 
     /**
      * 用户名
      */
+    @ApiModelProperty("用户名")
     private String username;
 
     /**
      * 0 admin 1 教师 2 学生
      */
+    @ApiModelProperty("0 admin 1 教师 2 学生")
     private Integer identity;
 
     /**
      * 密码
      */
+    @ApiModelProperty("密码")
     private String password;
 
     /**
      * 状态  0 启用 1 禁用
      */
+    @ApiModelProperty("状态  0 启用 1 禁用")
     private Integer state;
 
     /**
      * 头像
      */
+    @ApiModelProperty("头像")
     private String headPortrait;
 
     @TableField(exist = false)

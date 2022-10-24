f0f5756cae47d7902d353e88376f8b1a50d0f7b7
Suzysheep1106 <115514214+Suzysheep1106@users.noreply.github.com>
2022-10-24 14:29:13 +0800

    Update TheTestSystemApplicationTests.java
    测试生成的token的工具类

--- src/test/java/com/cxaou/thetestsystem/TheTestSystemApplicationTests.java
+++ src/test/java/com/cxaou/thetestsystem/TheTestSystemApplicationTests.java
@@ -2,6 +2,8 @@ package com.cxaou.thetestsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @SpringBootTest
 class TheTestSystemApplicationTests {
@@ -10,5 +12,14 @@ class TheTestSystemApplicationTests {
     void contextLoads() {
     }
 
-
+	@Autowired
+    private TokenUtil tokenUtil;
+    
+   @Test
+    void testToken(){
+        String sign = tokenUtil.sign(10002L);
+        System.out.println("sign = " + sign);
+        boolean verify = tokenUtil.verify(sign);
+        System.out.println("verify = " + verify);
+    }    
 }

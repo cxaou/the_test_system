89408a00c7e92942465b84d5ba3431e1588b3dc9
Suzysheep1106 <115514214+Suzysheep1106@users.noreply.github.com>
2022-10-24 20:29:22 +0800

    Update UserService.java
    修改bug

--- src/main/java/com/cxaou/thetestsystem/service/UserService.java
+++ src/main/java/com/cxaou/thetestsystem/service/UserService.java
@@ -1,37 +1,13 @@
-package com.cxaou.thetestsystem.service.impl;
+package com.cxaou.thetestsystem.service;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.cxaou.thetestsystem.erro.ParameterException;
 import com.cxaou.thetestsystem.pojo.User;
-import com.cxaou.thetestsystem.service.UserService;
-import com.cxaou.thetestsystem.mapper.UserMapper;
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.cxaou.thetestsystem.vo.LogVo;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 /**
  *
  */
-@Service
-public class UserServiceImpl extends ServiceImpl<UserMapper, User>
-    implements UserService{
+public interface UserService extends IService<User> {
 
-    @Override
-    public User login(LogVo user) {
-        try {
-            LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
-            //等于0 手机号登录
-            queryWrapper.eq(user.getType() == 0 && StringUtils.hasText(user.getPhone()), User::getPhone, user.getPhone())
-                    .eq(user.getType() == 1 && StringUtils.hasText(user.getUsername()), User::getUsername, user.getUsername());
-            User userOne = this.getOne(queryWrapper);
-            return userOne;
-        } catch (Exception e) {
-            throw new ParameterException("请求参数异常");
-        }
-    }
+    User login(LogVo user);
 }
-
-
-
-

20848392d706013595894a276a10dc1d7192a0ee
Suzysheep1106 <115514214+Suzysheep1106@users.noreply.github.com>
2022-10-24 08:41:15 +0800

    Update SwaggerConfig.java
    导入value包

--- src/main/java/com/cxaou/thetestsystem/config/SwaggerConfig.java
+++ src/main/java/com/cxaou/thetestsystem/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.cxaou.thetestsystem.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;

78e049624c764e717a6fd30023e8219201b49633
Suzysheep1106 <115514214+Suzysheep1106@users.noreply.github.com>
2022-10-25 09:58:30 +0800

    Update LogVo.java
    完善vo

--- src/main/java/com/cxaou/thetestsystem/vo/LogVo.java
+++ src/main/java/com/cxaou/thetestsystem/vo/LogVo.java
@@ -11,24 +11,24 @@ public class LogVo {
       /**
      * 手机号
      */
-	@ApiModelProperty("手机号")
+	@ApiModelProperty(value = "手机号",example = "15879461996")
     private String phone;
 
     /**
      * 用户名
      */
-	@ApiModelProperty("用户名") 
+	@ApiModelProperty(value = "用户名",example = "tom")
     private String username;
 
     /**
      * 密码
      */
-	@ApiModelProperty(value = "密码",required = true)
+	@ApiModelProperty(value = "密码",required = true,example = "000000")
     private String password;
 
     /**
      * 登录方式 0 手机号登录 1 用户名登录
      */
-	@ApiModelProperty(value = "登录方式 0 手机号登录 1 用户名登录",required = true)
+	@ApiModelProperty(value = "登录方式 0 手机号登录(只需传phone字段) 1 用户名登录(传入用户名字段)  手机字段跟用户名字段只选一个",required = true)
     private Integer type;  
 }

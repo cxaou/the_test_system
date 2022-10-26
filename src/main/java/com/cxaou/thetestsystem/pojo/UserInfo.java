409ed1213873e749274cdacd591215d7eb361c44
cxaou <2576353248@qq.com>
2022-10-26 20:51:04 +0800

    完成修改用户信息功能

--- src/main/java/com/cxaou/thetestsystem/pojo/UserInfo.java
+++ src/main/java/com/cxaou/thetestsystem/pojo/UserInfo.java
@@ -1,12 +1,12 @@
 package com.cxaou.thetestsystem.pojo;
 
-import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import java.io.Serializable;
-import java.util.Date;
 import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDate;
 
 /**
  * 
@@ -39,12 +39,12 @@ public class UserInfo implements Serializable {
     /**
      * 创建时间
      */
-    private Date createTime;
+    private LocalDate createTime;
 
     /**
      * 更新时间
      */
-    private Date updateTime;
+    private LocalDate updateTime;
 
     /**
      * 身份证号
--- src/main/java/com/cxaou/thetestsystem/service/impl/UserServiceImpl.java
+++ src/main/java/com/cxaou/thetestsystem/service/impl/UserServiceImpl.java
@@ -3,19 +3,29 @@ package com.cxaou.thetestsystem.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cxaou.thetestsystem.erro.ParameterException;
-import com.cxaou.thetestsystem.pojo.User;
-import com.cxaou.thetestsystem.service.UserService;
 import com.cxaou.thetestsystem.mapper.UserMapper;
+import com.cxaou.thetestsystem.pojo.User;
+import com.cxaou.thetestsystem.pojo.UserInfo;
+import com.cxaou.thetestsystem.service.UserInfoService;
+import com.cxaou.thetestsystem.service.UserService;
 import com.cxaou.thetestsystem.vo.LogVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
+
+import java.time.LocalDate;
 
 /**
  *
  */
+@Transactional
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService{
+
+    @Autowired
+    private UserInfoService userInfoService;
 
     @Override
     public User login(LogVo user) {
@@ -29,6 +39,16 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         } catch (Exception e) {
             throw new ParameterException("请求参数异常");
         }
+    }
+
+    @Override
+    public void signIn(LogVo user) {
+        this.save(user);
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserId(user.getId());
+        userInfo.setCreateTime(LocalDate.now());
+        userInfo.setUpdateTime(LocalDate.now());
+        userInfoService.save(userInfo);
     }
 }
 

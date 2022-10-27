e4640b6553a4563bf2eadb5de9db3d4a553202ac
cxaou <2576353248@qq.com>
2022-10-27 20:17:34 +0800

    增加mybatisPlus的分页插件

---
+++ src/main/java/com/cxaou/thetestsystem/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.cxaou.thetestsystem.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置mybatis-plus的配置类
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 配置mybatis-plus 的分页插件
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
